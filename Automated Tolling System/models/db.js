31ce2d976aa7f8b7daf5b3930e47ccb00a01bdb7
Omar Mokbel <o.mokbel69@gmail.com>
2019-06-22 14:08:17 +0200

    DB file update

--- Automated Tolling System/models/db.js
+++ Automated Tolling System/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://omarmokbel:bachelor123456@mycluster-c2yqc.mongodb.net/TollingSystem?retryWrites=true";
+const uri = "PLACE URI HERE";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   if (!err) { console.log('MongoDB Connection Successfull.') }
@@ -9,7 +9,7 @@ client.connect(err => {
   module.exports.collection = collection;
 });
 
-mongoose.connect('mongodb+srv://omarmokbel:bachelor123456@mycluster-c2yqc.mongodb.net/TollingSystem?retryWrites=true', { useNewUrlParser: true }, (err) => {
+mongoose.connect('PLACE URI HERE', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('Mongoose Connection Successfull.') }
     else { console.log('Error in Mongoose connection : ' + err) }
 });
